e91d9940f7f619ad02c3529cb7fe4e9146cb3f5f
PatKredatus <pat.kredatus@gmail.com>
2023-05-14 11:26:17 +0200

    login and sign-up

--- WhatToCook/urls.py
+++ WhatToCook/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from viewer.views import homepage
+from django.urls import path, include
+from viewer.views import logout_view, registration
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
+    path('accounts/logout/', logout_view),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('registration/', registration, name='registration'),
+    path('accounts/profile/', homepage, name='profile'),
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from viewer.forms import SignUpForm
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('homepage')
 
 
 def homepage(request):
